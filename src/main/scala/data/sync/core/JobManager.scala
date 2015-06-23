316a6ec37055e6bf8c97ff39b755eab59f79c0ac
heyee <kacyno@163.com>
2015-06-23 20:06:36 +0800

    demo

--- src/main/scala/data/sync/core/JobManager.scala
+++ src/main/scala/data/sync/core/JobManager.scala
@@ -3,6 +3,6 @@ package data.sync.core
 /**
  * Created by hesiyuan on 15/6/23.
  */
-class JobManager {
-  
+object JobManager {
+
 }
--- src/main/scala/data/sync/core/Scheduler.scala
+++ src/main/scala/data/sync/core/Scheduler.scala
@@ -5,7 +5,7 @@ import data.sync.common.ClusterMessages.TaskAttemptDesc
 /**
  * Created by hesiyuan on 15/6/23.
  */
-class Scheduler(bm:BeeManager,jm:JobManager) {
+object Scheduler {
   //获得任务分配
   def getAsigns():Array[(BeeId,TaskAttemptDesc)]={
     return Array((null,null))
